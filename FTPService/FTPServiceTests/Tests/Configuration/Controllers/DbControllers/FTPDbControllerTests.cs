a58c3b4065cee440a0dc5459d5f7e0f5c231f3e2
Maniek13 <mani3k1989@gmail.com>
2024-02-28 20:25:17 +0100

    testy

--- FTPService/FTPServiceTests/Tests/Configuration/Controllers/DbControllers/FTPDbControllerTests.cs
+++ FTPService/FTPServiceTests/Tests/Configuration/Controllers/DbControllers/FTPDbControllerTests.cs
@@ -2,12 +2,6 @@
 {
     internal class FTPDbControllerTests
     {
-        public async Task SetFTPConfigurationAsyncTests() => throw new NotImplementedException();
-        public async Task EditFTPConfigurationAsyncTests() => throw new NotImplementedException();
-        public async Task RemoveFTPConfigurationAsyncTests() => throw new NotImplementedException();
-        public async Task AddActionFolderAsyncTests() => throw new NotImplementedException();
-        public async Task EditActionFolderAsyncTests() => throw new NotImplementedException();
-        public async Task RemoveActionFolderAsyncTests() => throw new NotImplementedException();
         public void AddFileTests() => throw new NotImplementedException();
         public void DeleteFileTests() => throw new NotImplementedException();
     }
--- FTPService/FTPServiceTests/Tests/Configuration/Controllers/DbControllers/FTPDbROControllerTests.cs
+++ FTPService/FTPServiceTests/Tests/Configuration/Controllers/DbControllers/FTPDbROControllerTests.cs
@@ -2,11 +2,7 @@
 {
     internal class FTPDbROControllerTests
     {
-        public async Task GetPermision() => throw new NotImplementedException();
-        public async Task GetFTPConfiguration() => throw new NotImplementedException();
-        public async Task GetServiceActions() => throw new NotImplementedException();
         public async Task GetFile() => throw new NotImplementedException();
-        public async Task GetServiceAction() => throw new NotImplementedException();
         public async Task GetActionFiles() => throw new NotImplementedException();
     }
 }
